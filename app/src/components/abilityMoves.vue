6803d95ef2aa9e5b14092918d5142dc7c4940a64
WilsonD09 <wilsond34@nycstudents.net>
2025-05-20 12:31:31 -0400

    s

---
+++ app/src/components/abilityMoves.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="card" :class="elementClass">
+      <h3>{{ ability.name }}</h3>
+      <p>Element: {{ ability.element }}</p>
+      <p>Power: {{ ability.power }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,55 +1,47 @@
 <template>
-   <div><HomeInstruction></HomeInstruction></div>
+  <div><HomeInstruction></HomeInstruction></div>
 </template>
 
-
 <script setup lang="ts">
-import HomeInstruction from '@/components/HomeInstruction.vue'
+import HomeInstruction from '@/components/SignUp.vue'
 
-let number = 1;
-let multiplacationdmg = 2;
-let divisiondmg = 0;
-let additiondmg = 5;
-let subtractiondmg = 15.5;
+let number = 1
+let multiplacationdmg = 2
+let divisiondmg = 0
+let additiondmg = 5
+let subtractiondmg = 15.5
 
+function ice() {}
 
-function ice() {
-
-}
-
-function add(){
-number += additiondmg;
-console.log(number);
+function add() {
+  number += additiondmg
+  console.log(number)
 }
 
 function subtract() {
-number -= subtractiondmg;
-console.log(number);
+  number -= subtractiondmg
+  console.log(number)
 }
 function multiply() {
-number = number * multiplacationdmg;
-console.log(number);
+  number = number * multiplacationdmg
+  console.log(number)
 }
 function division() {
-    number = number / multiplacationdmg;
-    console.log(number);
-    }
+  number = number / multiplacationdmg
+  console.log(number)
+}
 function distort() {
-  number = number * (Math.random() * 3);
-  console.log(number);
-  }
+  number = number * (Math.random() * 3)
+  console.log(number)
+}
 function invert() {
-  number = -number;
-  console.log(number);
+  number = -number
+  console.log(number)
 }
-function  freezingDivide() {
+function freezingDivide() {
   division()
-  console.log(number);
+  console.log(number)
 }
-
-
-
-
 </script>
 
 <style scoped></style>
