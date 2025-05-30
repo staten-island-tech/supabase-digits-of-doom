cb055e5d6a5cf75a9764e69fdec3db2a8b022624
WilsonD09 <wilsond34@nycstudents.net>
2025-05-30 12:56:18 -0400

    Create authStore.ts

---
+++ app/src/stores/authStore.ts
@@ -0,0 +1,26 @@
+import { defineStore } from 'pinia';
+
+interface AuthState {
+  isLoggedIn: boolean;
+  user: any | null;
+  id: any | null;
+}
+
+export const useAuthStore = defineStore('auth', {
+  state: (): AuthState => ({
+    isLoggedIn: false,
+    user: null,
+    id: null,
+  }),
+  actions: {
+    setLoggedIn(id: any, user: any) {
+      this.isLoggedIn = true;
+      this.user = user;
+      this.id = id;
+    },
+    setLoggedOut() {
+      this.isLoggedIn = false;
+      this.user = null;
+    },
+  },
+});
